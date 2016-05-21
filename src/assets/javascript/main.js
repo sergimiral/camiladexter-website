e50daf0ad4942d1674f70b008cd9e8b8796e300b
sergimiral <sergimiral@gmail.com>
2016-05-21 19:56:03 +0200

    pongstgrm apparently cant handle it either…

---
+++ src/assets/javascript/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+
+  $('#selector').pongstgrm({
+    accessId:     '2050575330',
+    accessToken:  '2050575330.0ce33c3.97b6833818564d2cb0c89a30a2633b08',
+    show: 'liked'
+  });
+
+});
